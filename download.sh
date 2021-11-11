e9263f99d7d0c1e22eb48b918966d43381988920
Nathan Hammond <github.com@nathanhammond.com>
2021-11-11 20:02:27 +0800

    Update to grab more data.

--- download.sh
+++ download.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 function download {
-  curl -si https://vcd.cvpss.hk/data/$1.json > response.txt
+  curl -si https://vcd.cvpss.hk/data/$1.$2 > response.txt
 
   lastmodified=`cat response.txt | grep 'Last-Modified'`
   isodate=`node -e 'console.log(new Date(Date.parse(process.argv[1].replace(/^Last-Modified: /, ""))).toISOString())' "$lastmodified"`
 
   mkdir -p $1
-  tail -1 response.txt > "$1/$isodate.json"
+  tail -1 response.txt > "$1/$isodate.$2"
 
   rm response.txt
 }
 
-download summary
-download pie_age
-download pie_venue
-download pie_gender
-download bar_vaccination_date
+download summary json
+download pie_age json
+download pie_venue json
+download pie_gender json
+download bar_vaccination_date json
+download bar_age json
+download bar_venue json
+download data2a csv
+download data2b csv
+download data4a csv
+download data4b csv
 
 git add --all
 git commit -m `date +"%Y-%m-%dT%H:%M:%S%z"`
