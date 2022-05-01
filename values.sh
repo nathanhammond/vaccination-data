980dbb696b0cdba853fcbfba2bd67fcacf057ccc
k-joules <83689803+k-joules@users.noreply.github.com>
2022-05-01 23:43:19 +0800

    Update values.sh

--- values.sh
+++ values.sh
@@ -5,4 +5,4 @@
 type=$1
 
 cd bar_vaccination_date
-cat `ls -1 | tail -1` | jq --raw-output "[.[].firstDose.daily.$type], [.[].secondDose.daily.$type], [.[].thirdDose.daily.$type], [.[].fourthDose.daily.$type] [.[].firstDose.cumulative.$type], [.[].secondDose.cumulative.$type], [.[].thirdDose.cumulative.$type], [.[].fourthDose.cumulative.$type] | @tsv"
+cat `ls -1 | tail -1` | jq --raw-output "[.[].firstDose.daily.$type], [.[].secondDose.daily.$type], [.[].thirdDose.daily.$type], [.[].fourthDose.daily.$type], [.[].firstDose.cumulative.$type], [.[].secondDose.cumulative.$type], [.[].thirdDose.cumulative.$type], [.[].fourthDose.cumulative.$type] | @tsv"
