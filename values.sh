f1a1a78d0e1573f76ba13bc9d28a2a2b64f36072
k-joules <83689803+k-joules@users.noreply.github.com>
2022-05-01 23:36:32 +0800

    Update values.sh

--- values.sh
+++ values.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# May 1 2022 - Revised to include fourth dose (2nd booster) data -JC
 # November 12 2021 - Revised to include third dose (booster) data -JC
 
 type=$1
 
 cd bar_vaccination_date
-cat `ls -1 | tail -1` | jq --raw-output "[.[].firstDose.daily.$type], [.[].secondDose.daily.$type], [.[].thirdDose.daily.$type], [.[].firstDose.cumulative.$type], [.[].secondDose.cumulative.$type], [.[].thirdDose.cumulative.$type] | @tsv"
+cat `ls -1 | tail -1` | jq --raw-output "[.[].firstDose.daily.$type], [.[].secondDose.daily.$type], [.[].thirdDose.daily.$type], [.[].fourthDose.daily.$type] [.[].firstDose.cumulative.$type], [.[].secondDose.cumulative.$type], [.[].thirdDose.cumulative.$type], [.[].fourthDose.cumulative.$type] | @tsv"
